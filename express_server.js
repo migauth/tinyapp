a40a524e03ed62e0f3e063ba8826673b8c2badd6
Michael Gauthier <mikegauthier222@hotmail.com>
2023-11-27 14:28:11 -0800

    add new user object and use email as sign in name

--- express_server.js
+++ express_server.js
@@ -13,6 +13,14 @@ let urlDatabase = {
   "9sm5xK": "http://www.google.com",
 }
 
+// Set a base object for users
+const users = {
+  userRandomID: { id: "userRandomID", email: "user@example.com", password: "purple-monkey-dinosaur" },
+  user2RandomID: { id: "user2RandomID", email: "user2@example.com", password: "dishwasher-funk" }
+};
+
+
+
 // Function that generates random strings
 const random = function generateRandomString() {
   return Math.random().toString(36).substring(2,8);
@@ -45,21 +53,30 @@ app.post("/urls/:id", (req, res) => {
 // For login
 app.post("/login", (req, res) => {
   const user = req.body.user;
-  res.cookie('username', user);
+  res.cookie('user_id', user); //this needs to change to users_id
   res.redirect("/urls");
 })
 
 // For logout
 app.post("/logout", (req, res) => {
-  res.clearCookie('username');
+  res.clearCookie('user_id'); //this needs to change to users_id
   res.redirect("/urls");
 })
 
-// // For logout
-// app.post("/register", (req, res) => {
-//   res.clearCookie('username');
-//   res.redirect("/urls");
-// })
+// For register
+app.post("/register", (req, res) => { //this is where the random id generates
+  const ran = random();
+  const user = {
+      id: ran,
+      email: req.body.email,
+      password: req.body.password
+    }
+
+  users[`${ran}`] = user;
+  console.log(users);
+  res.cookie('user_id', ran)
+  res.redirect("/urls");
+})
 
 // Redirects user to longURL site when using id
 app.get("/u/:id", (req, res) => {
@@ -69,40 +86,50 @@ app.get("/u/:id", (req, res) => {
 
 // My URLS page
 app.get("/urls", (req, res) => {
+  let user_id = req.cookies["user_id"]
   const templateVars = { 
     urls: urlDatabase,
-    username: req.cookies["username"]
+    user: users[user_id]
+  
   };
+
   res.render("urls_index", templateVars);
 });
 
 // Create new tinyURL page
 app.get("/urls/new", (req, res) => {
+  let user_id = req.cookies["user_id"]
   const templateVars = { 
     urls: urlDatabase,
-    username: req.cookies["username"]
+    user: users[user_id]
+  
   };
   res.render("urls_new", templateVars);
 });
 
-// Edit page?
+// Edit page
 app.get("/urls/:id", (req, res) => {
+  let user_id = req.cookies["user_id"]
   const templateVars = { 
     id: req.params.id, 
     longURL: urlDatabase[`${req.params.id}`],
-    username: req.cookies["username"]}
-    ;
+    user: users[user_id]
+    };
   res.render("urls_show", templateVars);
 });
 
 // 
 app.get("/register", (req, res) => {
+  let user_id = req.cookies["user_id"]
   const templateVars = { 
     id: req.params.id, 
     longURL: urlDatabase[`${req.params.id}`],
-    username: req.cookies["username"]}
-    ;
-  res.render("register", templateVars);
+    user: users[user_id]
+    };
+
+    //insert the randomized id here?
+  
+    res.render("register", templateVars);
 });
 
 
